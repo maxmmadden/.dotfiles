d6fd7fcc2452dfec6028ba4eb84845c953bf424a
maxmmadden <mail2maxm13@gmail.com>
2025-03-24 20:54:33 +0000

    added blink cmp

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,70 +1,89 @@
 return {
+  {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+
+    version = "*",
+    opts = {
+      keymap = { preset = "super-tab" },
+
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+
+      completion = { documentation = { auto_show = false } },
+
+      signature = { enabled = true },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
   --{
   --	"github/copilot.vim",
   --},
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-buffer", -- source for text in buffer
-      "hrsh7th/cmp-path", -- source for file system paths
-      {
-        "L3MON4D3/LuaSnip",
-        version = "v2.*",
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
-      },
-      "rafamadriz/friendly-snippets",
-      "onsails/lspkind.nvim", -- vs-code like pictograms
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local lspkind = require("lspkind")
-      local luasnip = require("luasnip")
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
-          ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item() -- Move to the next item in the completion menu
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump() -- Expand or jump to the next snippet placeholder
-            else
-              fallback()        -- If no matches, fallback to default behavior
-            end
-          end, { "i", "s" }),
-        }),
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-      vim.cmd([[
-      set completeopt=menuone,noinsert,noselect
-      highlight! default link CmpItemKind CmpItemMenuDefault
-    ]])
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   event = "InsertEnter",
+  --   dependencies = {
+  --     "hrsh7th/cmp-buffer", -- source for text in buffer
+  --     "hrsh7th/cmp-path", -- source for file system paths
+  --     {
+  --       "L3MON4D3/LuaSnip",
+  --       version = "v2.*",
+  --       -- install jsregexp (optional!).
+  --       build = "make install_jsregexp",
+  --     },
+  --     "rafamadriz/friendly-snippets",
+  --     "onsails/lspkind.nvim", -- vs-code like pictograms
+  --     "hrsh7th/cmp-nvim-lsp",
+  --   },
+  --   config = function()
+  --     local cmp = require("cmp")
+  --     local lspkind = require("lspkind")
+  --     local luasnip = require("luasnip")
+  --
+  --     require("luasnip.loaders.from_vscode").lazy_load()
+  --
+  --     cmp.setup({
+  --       snippet = {
+  --         expand = function(args)
+  --           luasnip.lsp_expand(args.body)
+  --         end,
+  --       },
+  --       mapping = cmp.mapping.preset.insert({
+  --         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+  --         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  --         ["<C-Space>"] = cmp.mapping.complete(),
+  --         ["<C-e>"] = cmp.mapping.close(),
+  --         ["<CR>"] = cmp.mapping.confirm({
+  --           behavior = cmp.ConfirmBehavior.Replace,
+  --           select = true,
+  --         }),
+  --         ["<Tab>"] = cmp.mapping(function(fallback)
+  --           if cmp.visible() then
+  --             cmp.select_next_item() -- Move to the next item in the completion menu
+  --           elseif luasnip.expand_or_jumpable() then
+  --             luasnip.expand_or_jump() -- Expand or jump to the next snippet placeholder
+  --           else
+  --             fallback()        -- If no matches, fallback to default behavior
+  --           end
+  --         end, { "i", "s" }),
+  --       }),
+  --       window = {
+  --         completion = cmp.config.window.bordered(),
+  --         documentation = cmp.config.window.bordered(),
+  --       },
+  --       sources = cmp.config.sources({
+  --         { name = "nvim_lsp" },
+  --         { name = "luasnip" },
+  --         { name = "buffer" },
+  --         { name = "path" },
+  --       }),
+  --     })
+  --     vim.cmd([[
+  --     set completeopt=menuone,noinsert,noselect
+  --     highlight! default link CmpItemKind CmpItemMenuDefault
+  --   ]])
+  --   end,
+  -- },
 }
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,61 +1,75 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"towolf/vim-helm",
-		ft = "helm",
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "towolf/vim-helm",
+    ft = "helm",
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "saghen/blink.cmp",
+    },
+    lazy = false,
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			local util = require("lspconfig.util")
+      local util = require("lspconfig.util")
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.solargraph.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.solargraph.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
       lspconfig.jinja_lsp.setup({
         capabilities = capabilities,
       })
 
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-				cmd = { "gopls" },
-				filetypes = { "go", "gomod", "gowork", "gotmpl" },
-				root_dir = util.root_pattern("go.mod", ".git", "go.work"),
-				settings = {
-					gopls = {
-						completeUnimported = true,
-						usePlaceholders = true,
-						analyses = {
-							unusedparams = true,
-						},
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        cmd = { "gopls" },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_dir = util.root_pattern("go.mod", ".git", "go.work"),
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
             hints = {
               assignVariableTypes = true,
               compositeLiteralFields = true,
@@ -64,108 +78,107 @@ return {
               functionTypeParameters = true,
               parameterNames = true,
               rangeVariableTypes = true,
-					},
-				},
-			}})
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-			})
+            },
+          },
+        },
+      })
+      lspconfig.yamlls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.helm_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					["helm-ls"] = {
-						yamlls = {
-							path = "yaml-language-server",
-						},
-					},
-				},
-			})
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.helm_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          ["helm-ls"] = {
+            yamlls = {
+              path = "yaml-language-server",
+            },
+          },
+        },
+      })
 
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-				settings = {
-					["rust-analyzer"] = {
-						imports = {
-							granularity = {
-								group = "module",
-							},
-							prefix = "self",
-						},
-						cargo = {
-							buildScripts = {
-								enable = true,
-							},
-						},
-						procMacro = {
-							enable = true,
-						},
-					},
-				},
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
-
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            imports = {
+              granularity = {
+                group = "module",
+              },
+              prefix = "self",
+            },
+            cargo = {
+              buildScripts = {
+                enable = true,
+              },
+            },
+            procMacro = {
+              enable = true,
+            },
+          },
+        },
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
 
       vim.api.nvim_create_autocmd("BufWritePre", {
         callback = function()
-            vim.lsp.buf.format { async = false }
-        end
+          vim.lsp.buf.format({ async = false })
+        end,
       })
 
-
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*.go",
-				callback = function()
-					local params = vim.lsp.util.make_range_params()
-					params.context = { only = { "source.organizeImports" } }
-					local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-					for cid, res in pairs(result or {}) do
-						for _, r in pairs(res.result or {}) do
-							if r.edit then
-								local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-								vim.lsp.util.apply_workspace_edit(r.edit, enc)
-							end
-						end
-					end
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
-		end,
-	},
-	{
-		"simrat39/rust-tools.nvim",
-		lazy = false,
-		config = function()
-			require("rust-tools").setup({
-				tools = {
-					runnable = {
-						use_telescope = true,
-					},
-					inlay_hints = {
-						auto = true,
-						show_parameter_hints = false,
-						parameter_hints_prefix = "",
-						other_hints_prefix = "",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"numToStr/Comment.nvim",
-		opts = {
-			-- add any options here
-		},
-	},
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          local params = vim.lsp.util.make_range_params()
+          params.context = { only = { "source.organizeImports" } }
+          local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+          for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+              if r.edit then
+                local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                vim.lsp.util.apply_workspace_edit(r.edit, enc)
+              end
+            end
+          end
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    lazy = false,
+    config = function()
+      require("rust-tools").setup({
+        tools = {
+          runnable = {
+            use_telescope = true,
+          },
+          inlay_hints = {
+            auto = true,
+            show_parameter_hints = false,
+            parameter_hints_prefix = "",
+            other_hints_prefix = "",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+  },
 }
